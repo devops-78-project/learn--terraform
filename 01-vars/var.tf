8e43e2abca53b9b33677313215c8450e2c2ebdae
ganesh <desaiganeshkumar13@gmail.com>
2024-04-01 11:08:54 +0530

    create ec2 instances using terraform

--- 01-vars/var.tf
+++ 01-vars/var.tf
@@ -6,3 +6,23 @@ variable "x" {
 output "x" {
   value = var.x
 }
+
+variable "y_list" {
+  default = [10,20,30]
+}
+
+output "y" {
+  value = var.y_list
+}
+
+variable "z_map" {
+  default = {
+    x=10
+    y=20
+    z=40
+  }
+}
+
+output "z" {
+  value = var.z_map
+}
