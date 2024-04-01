8954947dc705376ce581bf5d8bf7076899c54851
ganesh <desaiganeshkumar13@gmail.com>
2024-04-01 09:17:03 +0530

    create ec2 instances using terraform

--- 03-expense-terraform/ec2.tf
+++ 03-expense-terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t2.micro"
-  vpc_security_group_ids =  sg-0585497ea4ba14da4
+  vpc_security_group_ids =  ["sg-0585497ea4ba14da4"]
   tags = {
     name = "frontend"
   }
@@ -10,7 +10,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.small"
-  vpc_security_group_ids = sg-0585497ea4ba14da4
+  vpc_security_group_ids = ["sg-0585497ea4ba14da4"]
   tags = {
     name = "backend"
   }
@@ -19,7 +19,7 @@ resource "aws_instance" "backend" {
 resource "aws_instance" "mysql" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.small"
-  vpc_security_group_ids = sg-0585497ea4ba14da4
+  vpc_security_group_ids = ["sg-0585497ea4ba14da4"]
   tags = {
     name = "mysql"
   }
