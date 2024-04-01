cfe25f0f049b57e8b22a367bc8e520b11c8de119
ganesh <desaiganeshkumar13@gmail.com>
2024-04-01 18:02:29 +0530

    added security group in data-source

--- 05-expense-terraform2/main.tf
+++ 05-expense-terraform2/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "frontend" {
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = var.security_group
   tags = {
-    name = each.key
+    Name = each.key
   }
 }
 
