5b3982dbd0ec36adeea1575d5c2b0bb084f49561
ganesh <desaiganeshkumar13@gmail.com>
2024-04-01 14:37:02 +0530

    created tfvars files in ec2 instances

--- 03-expense-terraform/ec2.tf
+++ 03-expense-terraform/ec2.tf
@@ -1,27 +1,39 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-05f020f5935e52dc4"
-  instance_type = "t2.micro"
-  vpc_security_group_ids =  ["sg-0585497ea4ba14da4"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
   tags = {
     name = "frontend"
   }
 }
 
 resource "aws_instance" "backend" {
-  ami           = "ami-05f020f5935e52dc4"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0585497ea4ba14da4"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
   tags = {
     name = "backend"
   }
 }
 
 resource "aws_instance" "mysql" {
-  ami           = "ami-05f020f5935e52dc4"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0585497ea4ba14da4"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
   tags = {
     name = "mysql"
   }
 
+}
+
+variable "ami" {
+  default = "ami-05f020f5935e52dc4"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "security_group" {
+  default = ["sg-0585497ea4ba14da4"]
 }
