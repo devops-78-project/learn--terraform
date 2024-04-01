f18ba9d7296246b196c4bcd529f3968f9b6ac108
ganesh <desaiganeshkumar13@gmail.com>
2024-04-01 17:58:37 +0530

    added security group in data-source

---
+++ 05-expense-terraform2/main.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "frontend" {
+  for_each= var.instance_types
+  ami           = var.ami
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = var.security_group
+  tags = {
+    name = each.key
+  }
+}
+
+variable "ami" {
+  default = "ami-05f020f5935e52dc4"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "security_group" {
+  default = ["sg-0585497ea4ba14da4"]
+}
+
+variable "instance_types" {
+  default = {
+    frontend ={
+      instance_type = "t3.micro"
+    }
+    mysql ={
+      instance_type = "t3.small"
+    }
+    backend ={
+      instance_type = "t3.micro"
+    }
+
+
+
+
+
+  }
+}
