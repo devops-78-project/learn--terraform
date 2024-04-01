cf12417eaeddddfb91d8a40e352448715e221e8f
ganesh <desaiganeshkumar13@gmail.com>
2024-04-01 17:26:01 +0530

    added security group in data-source

--- 04-module/main.tf
+++ 04-module/main.tf
@@ -1,7 +1,11 @@
 module "demo" {
   source = "./demo"
+  name = "demo-1"
+  instance_type = "t2.micro"
 }
 
 module "demo-1" {
   source = "./demo"
+  name = "demo-2"
+  instance_type = "t2.micro"
 }
