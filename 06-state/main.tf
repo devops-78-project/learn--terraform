8c5173392abe992a58389b4fa9dc318ff81a5828
ganesh <desaiganeshkumar13@gmail.com>
2024-04-24 12:27:22 +0530

    updating ami number in all modules

--- 06-state/main.tf
+++ 06-state/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket : "devops.tfvars"
-    key : "testing/state"
-    region : "us-east-1"
+    bucket = "devops.tfvars"
+    key = "testing/state"
+    region = "us-east-1"
 
   }
 }
